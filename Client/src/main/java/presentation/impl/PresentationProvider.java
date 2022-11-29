bfd6a3ddb855c8732e4036457f6534b3c9f4d1cb
UnitedFeodor <86407851+UnitedFeodor@users.noreply.github.com>
2022-11-29 19:46:25 +0300

    client

---
+++ Client/src/main/java/presentation/impl/PresentationProvider.java
@@ -0,0 +1,29 @@
+package presentation.impl;
+
+import presentation.UserInterface;
+import presentation.View;
+
+public class PresentationProvider {
+
+    private final View VIEW = new ViewImpl();
+    private final UserInterface USER_INTERFACE = new UserInterfaceImpl();
+    private static PresentationProvider instance;
+
+    private PresentationProvider() {
+    }
+
+    public static PresentationProvider getInstance() {
+        if (instance == null) {
+            instance = new PresentationProvider();
+        }
+        return instance;
+    }
+
+    public View getVIEW() {
+        return VIEW;
+    }
+
+    public UserInterface getUSER_INTERFACE() {
+        return USER_INTERFACE;
+    }
+}
